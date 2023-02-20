28097062910483d666ba80c31b2e8b43f0ee5034
hojun97 <rho510@naver.com>
2023-02-20 22:30:58 +0900

    kiki

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styled from "styled-components";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div>
+      <Bottom>
+        <Blog href="https://m.blog.naver.com/thstlgus0601">Blog</Blog>
+        <PlayList href="https://soundcloud.com/user-269111903/sets/ptubm01htmq2?ref=clipboard&p=i&c=1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
+          Playlist
+        </PlayList>
+      </Bottom>
+    </Div>
   );
 }
+const Div = styled.div`
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  display: flex;
+  font-weight: 700;
+  gap: 10px;
+  background-image: url(https://i.pinimg.com/564x/1f/be/b1/1fbeb1f5d9940a6610cf1de2c4e68ad9.jpg);
+  background-size: cover;
+  background-position: center;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  margin: 100px auto;
+  gap: 30px;
+  flex-direction: column;
+`;
+
+const Blog = styled.a`
+  text-decoration: none;
+  font-size: 36px;
+  color: white;
+  background-color: black;
+`;
+
+const PlayList = styled.a`
+  text-decoration: none;
+  color: black;
+  background-color: white;
+  font-size: 36px;
+`;
 
 export default App;
