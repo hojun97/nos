c587bf0bd2b841426d232a28d93c4d51d29845d9
hojun97 <rho510@naver.com>
2023-03-20 23:01:15 +0900

    고양이 스피너

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import styled from "styled-components";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { ReactComponent as Spinner } from "../src/catLoading.svg";
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [fadeOut, setFadeOut] = useState("");
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 4000);
+  }, []);
+
   return (
     <Div>
-      <Bottom>
-        <Blog href="https://m.blog.naver.com/thstlgus0601">Blog</Blog>
-        <PlayList href="https://soundcloud.com/user-269111903/sets/ptubm01htmq2?ref=clipboard&p=i&c=1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
-          Playlist
-        </PlayList>
-      </Bottom>
+      {loading ? (
+        <Bottom className="fade-in">
+          <Blog href="https://m.blog.naver.com/thstlgus0601">Blog</Blog>
+          <PlayList href="https://soundcloud.com/user-269111903/sets/ptubm01htmq2?ref=clipboard&p=i&c=1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing">
+            Playlist
+          </PlayList>
+        </Bottom>
+      ) : (
+        <SpinnerWrap className="fade-out">
+          <Spinner />
+        </SpinnerWrap>
+      )}
     </Div>
   );
 }
@@ -22,6 +38,45 @@ const Div = styled.div`
   background-image: url(https://i.pinimg.com/564x/1f/be/b1/1fbeb1f5d9940a6610cf1de2c4e68ad9.jpg);
   background-size: cover;
   background-position: center;
+
+  .fade-in {
+    opacity: 1;
+    animation: fadeIn ease-in-out 1s;
+  }
+
+  .fade-out {
+    opacity: 0;
+    animation: fadeOut ease-in-out 4s;
+  }
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.8;
+    }
+    100% {
+      opacity: 0.5;
+    }
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+const SpinnerWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: white;
 `;
 
 const Bottom = styled.div`
